53cb582da431bc5cfb837ff82fa304f2c74376d6
Warren Yao <warrenyca905@gmail.com>
2020-01-14 01:50:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
-const append = ["Milo", "Otis", "Garfield"];
+const append = ["Milo", "Otis", "Garfield"].unshift("Odie");
 const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
